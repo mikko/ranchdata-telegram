52dc9440db14d0b4543ccb0c443f6d7cc7b49c92
Mikko Pohja <mikko.pohja@futurice.com>
2017-09-24 22:44:02 +0300

    Login and latest values commands

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
+const fs = require('fs');
+const request = require('request-promise');
 const TelegramBot = require('node-telegram-bot-api');
+const Promise = require('bluebird');
 
 const telegramConfig = require('./config');
 
 const token = telegramConfig.apikey;
 const bot = new TelegramBot(token, {polling: true});
-
-const MAX_MESSAGE_LENGTH = 4096;
-const MAX_MESSAGES = 3;
 
 let myName;
 
@@ -15,58 +15,103 @@ bot.getMe().then(me => {
     console.log('My username is', myName);
 });
 
-const userStash = new Map();
+let userStash = new Map();
 
-const splitMessage = (msg, maxLength) => {
-    // const messageCount = parseInt(msg.length / maxLength) + 1;
-    const messages = [];
+const persistenceFile = 'users.json';
 
-    while(msg.length > 0) {
-        let cut = msg.lastIndexOf(' ', maxLength);
-        if (cut > MAX_MESSAGE_LENGTH) {
-            cut = msg.lastIndexOf(',', maxLength); // Assuming JSON response
+const savePool = () => {
+    const str = JSON.stringify(Array.from(userStash), null, 2);
+    console.log('Persisting client configs');
+    fs.writeFile(persistenceFile, str, err => {
+        if (err) {
+            console.log('Error persisting client pool', err);
         }
-        if (cut <= 0 && msg.length > 0) {
-            messages.push(msg);
-            break;
+    });
+};
+
+const loadPool = () => {
+    console.log('Loading client pool');
+    fs.readFile(persistenceFile, (err, data) => {
+        if (err) {
+            return;
         }
-        const messagePart = msg.slice(0, cut);
-        if (messagePart.length > 0) {
-            messages.push(messagePart);
-        }
-        msg = msg.substr(cut);
+        userStash = new Map(JSON.parse(data.toString()));
+    });
+};
+
+loadPool();
+
+const saveUserLogin = (id, apikey) => {
+    userStash.set(id, apikey);
+    savePool();
+};
+
+const serverHost = process.env.RANCH_SERVER || 'localhost:3000';
+
+const serverApi = `http://${serverHost}/api/v1`;
+
+const validateApiKey = token => request({
+    uri: `${serverApi}/apikeyTest`,
+    qs: {apikey: token},
+    json: true
+}).catch((err) => Promise.resolve(false));
+
+const getSensorList = userID => {
+    const token = userStash.get(userID);
+    if (!token) {
+        throw new Error('User not logged in');
     }
-    return messages;
+    return request({
+        uri: `${serverApi}/sensors`,
+        qs: {apikey: token},
+        json: true
+    }).catch((err) => Promise.resolve([]));
 };
 
-const serverUrl = 'asdasd/api/v1/apikeyTest';
-
-const validateApiKey = token => new Promise(resolve, reject) => {
-    const url = `${serverUrl}?apikey=${token}`;
+const getLatestValue = (userID, serial) => {
+    const token = userStash.get(userID);
+    if (!token) {
+        throw new Error('User not logged in');
+    }
+    return request({
+        uri: `${serverApi}/sensor/${serial}/latest`,
+        qs: {apikey: token},
+        json: true
+    }).catch((err) => Promise.resolve({}));
 };
+
+const helpText = `
+commands:
+/login apikey
+/status
+`;
 
 bot.onText(/\/start/, (msg, match) => {
-  bot.sendMessage(msg.chat.id, 'This is a bot for Ranchdata home monitoring system');
+    bot.sendMessage(msg.chat.id, 'This is a bot for Ranchdata home monitoring system');
+    setTimeout(() => bot.sendMessage(msg.chat.id, helpText), 1000);
+});
+
+bot.onText(/\/help/, (msg, match) => {
+    bot.sendMessage(msg.chat.id, helpText);
 });
 
 bot.onText(/\/status/, (msg, match) => {
-  bot.sendMessage(msg.chat.id, 'TODO');
-});
-
-bot.onText(/\/report[\s*](.*)/, (msg, match) => {
-  console.dir(match);
-  const hasParams = match.length > 1;
-  if (hasParams) {
-    const params = match[1].split(' ');
-    console.log(params);
-  }
-  bot.sendMessage(msg.chat.id, 'TODO ' + match);
+    getSensorList(msg.from.id)
+        .then(sensors => Promise.all(sensors.map(s => getLatestValue(msg.from.id, s.serial))))
+        .then(latestValues => bot.sendMessage(msg.chat.id,
+            latestValues.map(s => `${s.name} ${s.value}`).join('\n')));
 });
 
 bot.onText(/\/login[\s*](.*)/, (msg, match) => {
-  if (match.length > 1) {
-    const loginKey = match[1];
-    console.log(params);
-    bot.sendMessage(msg.chat.id, 'TODO login');
-  }
+    console.log(msg.from.id);
+    const apikey = match[1];
+    validateApiKey(apikey)
+        .then(isValid => {
+            if (isValid) {
+                bot.sendMessage(msg.from.id, 'Login successful');
+                saveUserLogin(msg.from.id, apikey);
+            } else {
+                bot.sendMessage(msg.from.id, 'Login failed');
+            }
+        });
 });
